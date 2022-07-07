abb2fb1d0c8628f00019965a1299e8149b035e72
Jennifer Grohe <jennifer.grohe@gmail.com>
2022-07-07 19:33:54 +0900

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,13 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:edit]
+
   def index
     @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
   end
 
   def new
@@ -10,21 +17,20 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
-      redirect_to restaurant_path(@restaurant)
+      redirect_to restaurant_path(@restaurant), notice: 'Restaurant was successfully created.'
     else
       render :new
     end
   end
 
-  def show
+  private
+
+  def set_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
 
-
-  private
-
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :rating, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :rating, :category)
   end
 
 end
